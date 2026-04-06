daf0a7ddd9a08370e7b9b7f118637b25176a8414
Panadir-cllr <20233tn204@utez.edu.mx>
2026-04-05 18:34:40 -0600

    fix urls.py backend
    Remove slashes from routes

--- backend/videogame_back/videogame_back/urls.py
+++ backend/videogame_back/videogame_back/urls.py
@@ -9,12 +9,12 @@ from . import views
 from user_profile import views as user_profile_views
 
 urlpatterns = [
-  path("admin/", admin.site.urls),
+  path("admin", admin.site.urls),
   path("api/health", views.health, name="health"),
   path("api/login", TokenObtainPairView.as_view(), name="token_obtain_pair"),
   path("api/token/refresh", TokenRefreshView.as_view(), name="token_refresh"),
   path("api/register", user_profile_views.register, name="register"),
-  path("api/", include("user_profile.urls")),
-  path("api/", include("inventory.urls")),
-  path("api/creatures/", include("creatures.urls")),
+  path("api", include("user_profile.urls")),
+  path("api", include("inventory.urls")),
+  path("api/creatures", include("creatures.urls")),
 ]
